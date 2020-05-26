e560e9a8e173a0e7e36e817ce308a90f2162a611
Kai Widman <kcwidman@gmail.com>
2020-05-26 20:21:46 +0000

    User controller set-up

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,10 +1,17 @@
 class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
+  belongs_to :receiver, class_name: "User" 
+  belongs_to :sender, class_name: "User" 
 
-  scope :between, -> (sender_id, receiver_id) do
+  scope :between, -> (sender_id, receiver_id) {
     where("(conversations.sender_id = ? AND conversations.receiver_id = ?) OR 
-    (conversations.sender_id = ? OR conversations.receiver_id = ?)", 
-    sender_id, receiver_id, receiver_id, sender_id)
+    (conversations.sender_id = ? AND conversations.receiver_id = ?)", 
+    sender_id, receiver_id, receiver_id, sender_id)}
+
+  #returns the name of the chat partner
+  def other_party_name(current_user_id)
+    return receiver.name if current_user_id == sender_id
+    sender.name
   end
 
 end
