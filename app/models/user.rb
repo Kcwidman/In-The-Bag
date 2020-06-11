e72cd15935c77bac8c27295e225ccb9d31851205
Kai Widman <kcwidman@gmail.com>
2020-06-11 20:45:39 +0000

    minor cleanup to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
   has_many :conversations
   has_many :messages
 
-  has_many :follows
   has_many :follower_relationships, foreign_key: :following_id, class_name: "Follow"
   has_many :followers, through: :follower_relationships, source: :follower
   has_many :following_relationships, foreign_key: :user_id, class_name: "Follow"
